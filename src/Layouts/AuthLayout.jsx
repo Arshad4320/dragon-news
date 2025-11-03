f58ac4e818b4d11b643fb9136df45e4d1f91e89c
Arshad Ullah <mdarshadullah835@gmail.com>
2025-11-03 12:57:11 +0600

    firbase config implemented

--- src/Layouts/AuthLayout.jsx
+++ src/Layouts/AuthLayout.jsx
@@ -4,11 +4,15 @@ import { Outlet } from "react-router-dom";
 
 const AuthLayout = () => {
   return (
-    <div>
-      <header>
+    <div className="relative min-h-screen bg-gray-50">
+      {/* Fixed Navbar */}
+      <header className="sticky top-0 left-0 w-full bg-white shadow-md z-50">
         <Navbar />
       </header>
-      <Outlet />
+
+      <main className="">
+        <Outlet />
+      </main>
     </div>
   );
 };
